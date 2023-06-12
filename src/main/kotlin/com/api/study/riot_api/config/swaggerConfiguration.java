779a31e796ff22c498bf31c7e9b4a900fd6f864b
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-06-12 23:46:13 +0900

    :sparkles: add swagger

---
+++ src/main/kotlin/com/api/study/riot_api/config/swaggerConfiguration.java
@@ -0,0 +1,24 @@
+package com.api.study.riot_api.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+
+public class swaggerConfiguration {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("라이엇 api")
+                .description("라이엇 api swagger")
+                .version("1.0.0");
+    }
+
+
+}
