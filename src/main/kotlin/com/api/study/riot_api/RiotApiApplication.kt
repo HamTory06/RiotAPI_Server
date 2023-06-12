1638d8955b2a5b275279235dec9437841d82084b
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-06-12 11:27:08 +0900

    :tada: add project

---
+++ src/main/kotlin/com/api/study/riot_api/RiotApiApplication.kt
@@ -0,0 +1,11 @@
+package com.api.study.riot_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RiotApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<RiotApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/api/study/riot_api/RiotApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.api.study.riot_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RiotApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
