07a54ca81c6c598d40641a1b1d57a2918552354a
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-06-17 20:11:32 +0900

    :zap: Login, signup logic processing

--- src/main/kotlin/com/api/study/riot_api/controller/AccountController.kt
+++ src/main/kotlin/com/api/study/riot_api/controller/AccountController.kt
@@ -1,27 +1,39 @@
 package com.api.study.riot_api.controller
 
+import com.api.study.riot_api.domain.dto.JwtDto
+import com.api.study.riot_api.domain.dto.LoginRequestDTO
+import com.api.study.riot_api.domain.dto.SignupRequestDTO
+import com.api.study.riot_api.service.LoginService
+import com.api.study.riot_api.service.SignupService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/api/account")
 class AccountController {
-    private val LOGGER: Logger = LoggerFactory.getLogger(AccountController::class.java)
+    @Autowired
+    private lateinit var signupService: SignupService
+    @Autowired
+    private lateinit var loginService: LoginService
     @PostMapping("/login")
     fun login(
-        @RequestParam id: String,
-        @RequestParam password: String
-    ): String{
-        LOGGER.info("id: $id\n password: $password")
-        return "id: $id\n password: $password"
+        @RequestBody loginRequestDTO: LoginRequestDTO
+    ): JwtDto {
+        loginService.execute(loginRequestDTO)
+
+        return loginService.execute(loginRequestDTO)
     }
 
     @PostMapping("/signup")
-    fun signup(){
-
+    fun signup(
+        @RequestBody @Valid signupRequestDTO: SignupRequestDTO
+    ) {
+        signupService.save(signupRequestDTO)
     }
 }
--- src/main/kotlin/com/api/study/riot_api/repository/AccountRepository.kt
+++ src/main/kotlin/com/api/study/riot_api/repository/AccountRepository.kt
@@ -3,8 +3,11 @@ package com.api.study.riot_api.repository
 import com.api.study.riot_api.domain.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
-public interface AccountRepository: JpaRepository<User, Long> {
+interface AccountRepository: JpaRepository<User, Long> {
+
+    fun findById(id: String): Optional<User>;
 
 }
