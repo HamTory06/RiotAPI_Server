cd5f029c8ed2f8cef16b1f7228d49c17d34e4bd4
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-06-17 20:01:06 +0900

    :sparkles: login logic

---
+++ src/main/kotlin/com/api/study/riot_api/domain/dto/LoginRequestDTO.kt
@@ -0,0 +1,10 @@
+package com.api.study.riot_api.domain.dto
+
+import javax.validation.constraints.NotBlank
+
+data class LoginRequestDTO(
+    @field:NotBlank(message = "id가 비어있습니다")
+    val id: String,
+    @field:NotBlank(message = "password가 비어있습니다")
+    val password: String
+)
---
+++ src/main/kotlin/com/api/study/riot_api/service/LoginService.kt
@@ -0,0 +1,36 @@
+package com.api.study.riot_api.service
+
+import com.api.study.riot_api.config.SecurityConfig
+import com.api.study.riot_api.controller.AccountController
+import com.api.study.riot_api.domain.dto.JwtDto
+import com.api.study.riot_api.domain.dto.LoginRequestDTO
+import com.api.study.riot_api.domain.entity.User
+import com.api.study.riot_api.exception.CustomException
+import com.api.study.riot_api.exception.ErrorCode
+import com.api.study.riot_api.repository.AccountRepository
+import com.api.study.riot_api.security.JwtToken
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.stereotype.Service
+
+@Service
+class LoginService(
+    private var accountRepository: AccountRepository,
+    private var securityConfig: SecurityConfig,
+    private var jwtToken: JwtToken
+) {
+    private val LOGGER: Logger = LoggerFactory.getLogger(AccountController::class.java)
+
+    fun execute(requestDTO: LoginRequestDTO): JwtDto {
+        val user: User = accountRepository.findById(requestDTO.id)
+            .orElseThrow { CustomException(ErrorCode.USER_NOT_FOUND_BAD_REQUEST) }
+
+        if (!securityConfig.passwordEncoder().matches(requestDTO.password, user.password)) {
+            throw CustomException(ErrorCode.PASSWORD_NOT_FOUND_BAS_REQUEST)
+        }
+
+        return JwtDto(jwtToken.makeJwtToken(user.idx, user.mail))
+    }
+
+}
