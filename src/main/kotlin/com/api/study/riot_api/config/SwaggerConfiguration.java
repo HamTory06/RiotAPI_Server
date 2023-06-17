0855e4d4951b353520b6ec8947dc28a065e53358
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-06-17 19:46:39 +0900

    :memo: add Swagger

--- src/main/kotlin/com/api/study/riot_api/config/SwaggerConfiguration.java
+++ src/main/kotlin/com/api/study/riot_api/config/SwaggerConfiguration.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 
-public class swaggerConfiguration {
+public class SwaggerConfiguration {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
