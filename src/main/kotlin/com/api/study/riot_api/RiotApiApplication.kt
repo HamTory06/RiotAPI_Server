7bdea208e662bc86e2268aec837703b257c559e1
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-08-30 20:28:12 +0900

    :sparkles: :: Use ConfigurationProperties

--- src/main/kotlin/com/api/study/riot_api/RiotApiApplication.kt
+++ src/main/kotlin/com/api/study/riot_api/RiotApiApplication.kt
@@ -1,11 +1,14 @@
 package com.api.study.riot_api
 
+import com.api.study.riot_api.infrastructure.security.properties.SecurityProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableConfigurationProperties(SecurityProperties::class)
 class RiotApiApplication
 
 fun main(args: Array<String>) {
