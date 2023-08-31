013587c9a2e66313718bd361f8fceb2f3a0f68dd
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-08-31 22:07:05 +0900

    :zap: :: add passwordEncoder

--- src/main/kotlin/com/api/study/riot_api/infrastructure/security/config/SecurityConfig.kt
+++ src/main/kotlin/com/api/study/riot_api/infrastructure/security/config/SecurityConfig.kt
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
@@ -25,4 +27,7 @@ class SecurityConfig {
             .anyRequest().authenticated()
         return http.build()
     }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
 }
