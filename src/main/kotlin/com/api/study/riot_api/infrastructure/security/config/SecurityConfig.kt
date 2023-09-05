71ee7652da9f77bfe07982e5bfcf69f39455277a
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-09-05 23:29:48 +0900

    :bug: :: Fixed bug where swagger was blocked by filters

--- src/main/kotlin/com/api/study/riot_api/infrastructure/security/config/SecurityConfig.kt
+++ src/main/kotlin/com/api/study/riot_api/infrastructure/security/config/SecurityConfig.kt
@@ -1,49 +1,1 @@
-package com.api.study.riot_api.infrastructure.security.config
-
-import com.api.study.riot_api.global.filter.JwtFilter
-import com.api.study.riot_api.infrastructure.security.properties.SecurityProperties
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.core.annotation.Order
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
-import org.springframework.stereotype.Component
-
-
-@Configuration
-@EnableWebSecurity
-class SecurityConfig(
-    private val jwtFilter: JwtFilter
-) {
-
-    @Bean
-    protected fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
-        http
-            .csrf().disable()
-            .httpBasic().disable()
-            .cors()
-            .and()
-
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-
-            .and()
-            .authorizeHttpRequests()
-            .antMatchers("/swagger-ui/**", "/v3/**").permitAll()
-            .anyRequest().authenticated()
-
-            .and()
-            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter::class.java)
-
-        return http.build()
-    }
-
-
-
-    @Bean
-    protected fun passwordEncoder() = BCryptPasswordEncoder()
-}
+package com.api.study.riot_api.infrastructure.security.configimport com.api.study.riot_api.global.filter.JwtFilterimport com.api.study.riot_api.infrastructure.security.properties.SecurityPropertiesimport lombok.RequiredArgsConstructorimport org.springframework.context.annotation.Beanimport org.springframework.context.annotation.Configurationimport org.springframework.http.HttpMethodimport org.springframework.security.config.annotation.web.builders.HttpSecurityimport org.springframework.security.config.annotation.web.builders.WebSecurityimport org.springframework.security.config.annotation.web.configuration.EnableWebSecurityimport org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizerimport org.springframework.security.config.http.SessionCreationPolicyimport org.springframework.security.crypto.bcrypt.BCryptPasswordEncoderimport org.springframework.security.web.SecurityFilterChainimport org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter@Configuration@EnableWebSecurity@RequiredArgsConstructorclass SecurityConfig(    val securityProperties: SecurityProperties) {    @Bean    fun webSecurityCustomizer(): WebSecurityCustomizer {        return WebSecurityCustomizer { web: WebSecurity ->            web.ignoring().antMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**")        }    }    @Bean    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {        http            .csrf().disable()            .cors()//            .and()//            .sessionManagement()//            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)            .and()            .authorizeRequests()            //auth            .antMatchers("/api/auth/**").permitAll()            //user//            .antMatchers("/api/user/delete-user/{id}").permitAll()            .anyRequest().authenticated()            .and()            .addFilterBefore(JwtFilter(securityProperties), UsernamePasswordAuthenticationFilter::class.java)        return http.build()    }    @Bean    protected fun passwordEncoder() = BCryptPasswordEncoder()}
