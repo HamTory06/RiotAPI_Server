38b3cf90faa92695e67a0f6b1d15e0f074481419
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-09-09 13:44:03 +0900

    :zap: :: Course error resolution

---
+++ src/main/kotlin/com/api/study/riot_api/infrastructure/config/WebMvcConfig.kt
@@ -0,0 +1,14 @@
+package com.api.study.riot_api.infrastructure.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+@Configuration
+class WebMvcConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("*")
+            .allowedHeaders("headers")
+    }
+}
